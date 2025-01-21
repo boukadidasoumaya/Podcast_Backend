2bac9d54916a48d1d3b4e78f37f5a617510e7e3f
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-21 22:29:39 +0100

    small update

--- src/episode/dto/create-episode.dto.ts
+++ src/episode/dto/create-episode.dto.ts
@@ -3,15 +3,11 @@ import { IsString, IsBoolean, IsNumber, IsNotEmpty } from 'class-validator';
 export class CreateEpisodeDto {
   @IsString()
   @IsNotEmpty()
-  name: string; // Name of the episode
+  Title: string; // Name of the episode
 
   @IsNumber()
   @IsNotEmpty()
   number: number; // Episode number
-
-  @IsBoolean()
-  @IsNotEmpty()
-  premium: boolean; // Whether the episode is premium
 
   @IsNumber()
   @IsNotEmpty()
