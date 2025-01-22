3915e617bcdce935050d76da4978b4e5724d9dd6
siwarZidi <siwar.zidi@insat.ucar.tn>
2025-01-22 18:08:43 +0100

    owner entity

--- src/owner/entities/owner.entity.ts
+++ src/owner/entities/owner.entity.ts
@@ -1,1 +1,8 @@
-export class Owner {}
+import { PartialType } from "@nestjs/mapped-types";
+import { User } from "src/user/entities/user.entity";
+import { Column } from "typeorm";
+
+export class Owner extends PartialType(User) {
+    @Column() 
+    interests: string[];
+}
