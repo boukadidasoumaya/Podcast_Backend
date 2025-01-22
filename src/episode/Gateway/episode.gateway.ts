7cc1896e68839b03d2af2710a601a713a9ac5a11
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-22 18:47:04 +0100

    test

--- src/episode/Gateway/episode.gateway.ts
+++ src/episode/Gateway/episode.gateway.ts
@@ -1,11 +1,22 @@
-import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-
+import { WebSocketGateway, OnGatewayInit, WebSocketServer, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 import { Episode } from '../entities/episode.entity';
-@WebSocketGateway()
-export class EpisodeGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  @WebSocketServer() server: Server;
+@WebSocketGateway( { cors: '*' }
+  
+)
 
+export class EpisodeGateway implements OnGatewayInit {
+    @WebSocketServer()
+    server: Server;
+  
+    afterInit(server: Server) {
+      console.log('WebSocket server initialized');
+    }
+  
+    @SubscribeMessage('episodeUpdate')
+    handleEpisodeUpdate(@MessageBody() data: any) {
+      this.server.emit('episodeUpdate', data); // Emit the event to all connected clients
+    }
   // Notify clients about episode data updates
   notifyEpisodeUpdate(episode: Episode) {
     this.server.emit('episodeUpdate', episode);  // Send the full episode data to clients
