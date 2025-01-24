10f5e28828d9c152ba0b09dc73e921b613fa941d
arijzaidi <arij.zaidi@insat.ucar.tn>
2025-01-24 17:43:27 +0100

    mail fixed

--- src/podcast/podcast.controller.ts
+++ src/podcast/podcast.controller.ts
@@ -14,12 +14,15 @@ import { PodcastService } from './podcast.service';
 import { CreatePodcastDto } from './dto/create-podcast.dto';
 import { UpdatePodcastDto } from './dto/update-podcast.dto';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { ApiBearerAuth, ApiOkResponse } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
 
 @Controller('podcast')
 export class PodcastController { 
   constructor(private readonly podcastService: PodcastService) {}
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @Post()
   create(@Body() createPodcastDto: CreatePodcastDto) {
     return this.podcastService.create(createPodcastDto);
@@ -46,11 +49,13 @@ export class PodcastController {
     return this.podcastService.findOne(+id);
   }
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePodcastDto: UpdatePodcastDto) {
     return this.podcastService.update(+id, updatePodcastDto);
   }
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.podcastService.remove(+id);
@@ -58,12 +63,14 @@ export class PodcastController {
 
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @Post(':p_id/subscribe')
   subscribe(@Param('p_id') p_id: string, @Req() req) {
     const u_id = req.user.id;
     return this.podcastService.subscribe(+u_id,+p_id);
   }
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @Post(':p_id/unsubscribe')
   unsubscribe(@Param('p_id') p_id: string, @Req() req) {
     const u_id = req.user.id;
